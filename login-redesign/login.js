d5a030accf5d30209fa037853a8e58c49faeecaf
Eric Tendian <erictendian@gmail.com>
2014-10-27 13:18:44 -0500

    Completed initial login form

--- login-redesign/login.js
+++ login-redesign/login.js
@@ -2,23 +2,22 @@ jQuery.noConflict();
 (function ($) {
     $(function () {
         $('html').removeClass('no-js');
-        $('#login-content .signup a').click(function (event) {
-            event.preventDefault();
-            $('#login-content').hide();
-            $('#signup-content').show();
-            var pagetitle = $('#signup-content').attr('data-pagetitle');
-            $('head > title').text(pagetitle);
-            window.history.pushState(pagetitle, pagetitle, "signup.html");
+        $('#register').click(function (e) {
+            if ($(this).is(':checked')) {
+                $('#logo #logintext').hide();
+                $('#logo #signuptext').show();
+            } else {
+                $('#logo #logintext').show();
+                $('#logo #signuptext').hide();
+            }
+            var newvalue = $('#logo .action').attr('alttext');
+            $('#logo .action').attr('alttext', $('#logo .action').text());
+            $('#logo .action').text(newvalue);
+            $('head > title').text($('#logo h1').text());
+            var newvalue = $('form .submit').attr('alt-value');
+            $('form .submit').attr('alt-value', $('form .submit').attr('value'));
+            $('form .submit').attr('value', newvalue);
         });
-        $('#signup-content .login a').click(function (event) {
-            event.preventDefault();
-            $('#login-content').show();
-            $('#signup-content').hide();
-            var pagetitle = $('#login-content').attr('data-pagetitle');
-            $('head > title').text(pagetitle);
-            window.history.pushState(pagetitle, pagetitle, "login.html");
-        });
-        $('#login-content .submit').addClass('avoid-clicks');
     });
 })(jQuery);
 
---
+++ login-redesign/self-critique.md
@@ -0,0 +1,5 @@
+# Self-Critique
+## Login Form Redesign
+### Oct. 31, 2014
+
+[TBA]
